3af99b6813b696a82475f150c7bc44739c3979f7
Domenico159 <domenico.zummo@gmail.com>
2021-06-03 14:42:36 +0200

    Add validation name

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 
 
-const userName = prompt('Inserisci il tuo nome');
-const userLastName = prompt('Inserisci il tuo cognome');
+
+let userName = prompt('Inserisci il tuo nome').trim();
+
+while ((!isNaN(userName) || userName == '')) {
+    userName = prompt('Nome non valido , prego riprovare').trim();
+}
+
+let userLastName = prompt('Inserisci il tuo cognome').trim();
+
+while ((!isNaN(userLastName) || userLastName == '')) {
+    userLastName = prompt('Cognome non valido , prego riprovare').trim();
+}
 
 
 
