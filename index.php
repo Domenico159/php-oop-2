c8c08c29a0f1d32c9e18ec7955284277a0c5a6cb
Domenico159 <domenico.zummo@gmail.com>
2021-06-03 14:55:52 +0200

    Project beta 1.1

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ require_once __DIR__ . '/data/main.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tecno-commerce</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="./css/main.css">
 </head>
 
@@ -40,7 +39,6 @@ require_once __DIR__ . '/data/main.php';
                     <p> <?php echo $item->getText() ?>... </p>
                     <h2>Prezzo <?php echo $item->price ?>€</h2>
                     <h2> <?php echo $item->getPrice($loginID, $users_premium) ?> </h2>
-                    <i class="fas fa-cart-plus"></i>
                 </div>
             </div>
 
